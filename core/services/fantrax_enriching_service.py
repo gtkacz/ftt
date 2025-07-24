e9e773b2d2027021094a97e3e9cb94d48b58e28d
gtkacz <gabriel@tkacz.dev.br>
2025-07-24 14:16:38 -0300

    chore: data update

--- core/services/fantrax_enriching_service.py
+++ core/services/fantrax_enriching_service.py
@@ -27,7 +27,7 @@ def main(csv_path):
 			# Find matching player
 			player = None
 			for p in Player.objects.all():
-				full_name = f"{p.first_name} {p.last_name}".removesuffix(" Jr").removesuffix(" Jr.")
+				full_name = f"{p.first_name} {p.last_name}".removesuffix(" Jr").removesuffix(" Jr.").replace("Cam", "Cameron")
 				if unidecode.unidecode(full_name) == norm_csv_name:
 					player = p
 					break
